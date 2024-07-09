39f907006bf5b9e6848e65f499d35775f0fef9d6
smart-life-tech <christlightworld@gmail.com>
2024-07-09 14:22:50 +0100

    modified:   juke3.ino

--- juke3.ino
+++ juke3.ino
@@ -394,6 +394,8 @@ void continuePlaying(int play)
 
 void continuePlayingLong()
 {
+    digitalWrite(27, LOW);
+    digitalWrite(28, LOW);
     if (longPressed)
     {
         bool busyPinState = digitalRead(busyPin); // read the busy pin
