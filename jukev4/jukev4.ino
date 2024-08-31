f83b7c8fe3e8ff415978f874cd8035971cfc0532
smart-life-tech <christlightworld@gmail.com>
2024-08-31 22:25:46 +0100

    modified:   jukev4/jukev4.ino

--- jukev4/jukev4.ino
+++ jukev4/jukev4.ino
@@ -410,7 +410,8 @@ void playTheList()
             if (busyPinState == 1 && !keypadLong) // has it gone from low to high?, meaning the track finished
             {
                 lastPlayed = sequenceList[playIndex];
-                if ((lastPlayed >= 100 && lastPlayed < 180) || (lastPlayed >= 200 && lastPlayed < 280))
+                //  if ((lastPlayed >= 100 && lastPlayed < 180) || (lastPlayed >= 200 && lastPlayed < 280))
+                if (1)
                 {
                     Serial.print("playing number  = ");
                     Serial.println(sequenceList[playIndex]);
