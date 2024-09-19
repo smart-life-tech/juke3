59badf98da29532712069ceec0130f21603373a6
christolightTech <58917014+smart-life-tech@users.noreply.github.com>
2024-09-19 14:43:50 +0100

    Update jukev4.ino

--- jukev4/jukev4.ino
+++ jukev4/jukev4.ino
@@ -61,7 +61,7 @@ bool playList = true;
 static bool lastBusyPinState = 0;
 byte currentDisplayLine = 1;
 bool keypadLong = false;
-#define MAX_SEQUENCE_LENGTH 1000 // Maximum length of the sequence list
+#define MAX_SEQUENCE_LENGTH 10000 // Maximum length of the sequence list
 bool numAlpha = false;
 
 int sequenceList[MAX_SEQUENCE_LENGTH]; // Array to store the sequence list
@@ -232,7 +232,7 @@ void addToSequenceList(int trackNumber)
         sequenceList[sequenceLength] = trackNumber;
         sequenceLength++;
         musicCount++;
-        for (int i = trackNumber; i < (trackNumber + 100); i++)
+        for (int i = trackNumber; i < (trackNumber + 1000); i++)
         {
             sequenceList[sequenceLength] = i;
             sequenceLength++;
