4110f410f7adde1e74e72bc216a3dd47fdaba196
smart-life-tech <christlightworld@gmail.com>
2024-11-10 21:25:46 +0100

    modified:   code444/code444.ino

--- code444/code444.ino
+++ code444/code444.ino
@@ -407,7 +407,7 @@ void continuePlayingLong()
 
         bool busyPinState = digitalRead(busyPin); // read the busy pin
         digitalWrite(A15, HIGH);
-        if (digitalRead(busyPin) == 1 || myDFPlayer.readState() == -1 ) // has it gone from low to high?, meaning the track finished
+        if (digitalRead(busyPin) == 1   ) // has it gone from low to high?, meaning the track finished
         {
             Serial.println("song not selected starting a new song");
             Serial.print("playing number in readstate1 = ");
