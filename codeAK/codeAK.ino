e721e49c7a8096b1df63f4e09e7f14a1787a9e53
christolightTech <58917014+smart-life-tech@users.noreply.github.com>
2025-12-02 00:51:00 +0100

    Flush and end mp3Serial before playback

--- codeAK/codeAK.ino
+++ codeAK/codeAK.ino
@@ -603,9 +603,9 @@ void handleNumberPress(int index)
         Serial.println("Starting playback from queue immediately after number entered.");
         // playSong(queue[currentPlaying].letter, queue[currentPlaying].number);
         play = true;
-        mp3Serial.end();
-        mp3Serial=NULL;
         
+        mp3Serial.flush();
+        mp3Serial.end();
         // currentPlaying = 1;
         EEPROM.write(EEPROM_BECKON_FLAG_ADDR, 0);
         EEPROM.write(EEPROM_RESET_FLAG_ADDR, 1);
