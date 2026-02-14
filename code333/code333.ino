e8a0d8105ad55c0f204cbfb9ce76ed74c220acba
smart-life-tech <christlightworld@gmail.com>
2026-02-14 19:53:45 +0100

    `Added beckon track array and modified beckon index handling`

--- code333/code333.ino
+++ code333/code333.ino
@@ -125,6 +125,29 @@ unsigned long lastBeckonTime = 0;
 unsigned long lastActivityTime = 0;
 int beckonIndex = 0;
 bool beckonPlaying = false;
+const int beckonTracks[] = {
+    100, 101, 102, 103, 104, 105, 106, 107,
+    110, 111, 112, 113, 114, 115, 116, 117,
+    120, 121, 122, 123, 124, 125, 126, 127,
+    130, 131, 132, 133, 134, 135, 136, 137,
+    140, 141, 142, 143, 144, 145, 146, 147,
+    150, 151, 152, 153, 154, 155, 156, 157,
+    160, 161, 162, 163, 164, 165, 166, 167,
+    170, 171, 172, 173, 174, 175, 176, 177,
+    180, 181, 182, 183, 184, 185, 186, 187,
+    190, 191, 192, 193, 194, 195, 196, 197,
+    200, 201, 202, 203, 204, 205, 206, 207,
+    210, 211, 212, 213, 214, 215, 216, 217,
+    220, 221, 222, 223, 224, 225, 226, 227,
+    230, 231, 232, 233, 234, 235, 236, 237,
+    240, 241, 242, 243, 244, 245, 246, 247,
+    250, 251, 252, 253, 254, 255, 256, 257,
+    260, 261, 262, 263, 264, 265, 266, 267,
+    270, 271, 272, 273, 274, 275, 276, 277,
+    280, 281, 282, 283, 284, 285, 286, 287,
+    290, 291, 292, 293, 294, 295, 296, 297
+};
+const int beckonTrackCount = sizeof(beckonTracks) / sizeof(beckonTracks[0]);
 
 void songSelectionTrigger()
 {
@@ -265,15 +288,9 @@ void startLightShow()
 
 int beckonIndexToTrack(int index)
 {
-    int letter = index / 10;
-    int number = index % 10;
-    int displayNumber = number + 1;
-    int trackNumber = (letter * 10) + displayNumber; // 1..100
-    if (trackNumber < 1)
-        trackNumber = 1;
-    if (trackNumber > 100)
-        trackNumber = 100;
-    return trackNumber;
+    if (index < 0 || index >= beckonTrackCount)
+        index = 0;
+    return beckonTracks[index];
 }
 
 void playBeckonTrack()
@@ -292,7 +309,7 @@ void playBeckonTrack()
     lastBeckonTime = millis();
     lastActivityTime = millis();
 
-    beckonIndex = (beckonIndex + 1) % 100;
+    beckonIndex = (beckonIndex + 1) % beckonTrackCount;
     EEPROM.write(EEPROM_BECKON_INDEX_ADDR, beckonIndex);
 }
 
@@ -1110,7 +1127,7 @@ void setup()
     digitalWrite(inhibitPin, LOW); // Start with inhibit disabled (0v)
 
     int savedBeckonIndex = EEPROM.read(EEPROM_BECKON_INDEX_ADDR);
-    if (savedBeckonIndex < 0 || savedBeckonIndex > 99)
+    if (savedBeckonIndex < 0 || savedBeckonIndex >= beckonTrackCount)
         savedBeckonIndex = 0;
     beckonIndex = savedBeckonIndex;
     swiped = (EEPROM.read(EEPROM_SWIPED_FLAG_ADDR) == 1);
